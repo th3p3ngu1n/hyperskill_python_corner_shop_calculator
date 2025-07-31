22ff38e135c2475470139a810960f7edb5c15d00
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-07-31 08:06:07 -0500

    stage 2 complete

--- corner_shop_calculator.py
+++ corner_shop_calculator.py
@@ -7,10 +7,30 @@ prices = {
     "Pancake": 3.2,
 }
 
+earnings = {
+    "Bubblegum": 202,
+    "Toffee": 118,
+    "Ice cream": 2250,
+    "Milk chocolate": 1680,
+    "Doughnut": 1075,
+    "Pancake": 80,
+}
 
-def stage_1(prices):
+
+def display_item_and_money(shop_items: dict[str, float]) -> str:
+    print("\n".join([f"{item}: ${value}" for item, value in shop_items.items()]))
+
+
+def stage_1(prices: dict[str, float]):
     print("Prices:")
-    print("\n".join([f"{item}: ${price}" for item, price in prices.items()]))
+    display_item_and_money(prices)
 
 
-stage_1(prices)
+def stage_2(earnings: dict[str, float]):
+    print("Earned amount:")
+    display_item_and_money(earnings)
+    income = sum(earnings.values())
+    print(f"\nIncome: ${income:.1f}")
+
+
+stage_2(earnings)
